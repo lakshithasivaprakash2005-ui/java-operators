c3492859e8641bffcb512da123e4b5737f3095aa
lakshithasivaprakash2005-ui <lakshithasivaprakash2005@gmail.com>
2025-12-11 18:16:37 +0530

    Add files via upload

---
+++ avg.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class main 
+{
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+       int a=2;
+       int b=3;
+       int c=4;
+       int d=a+b+c;
+        System.out.println(d/3);
+    }
+}
