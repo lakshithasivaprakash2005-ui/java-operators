39ad75569647f4ed31de5bc6d3dcf250ff4d6762
lakshithasivaprakash2005-ui <lakshithasivaprakash2005@gmail.com>
2025-12-11 18:12:53 +0530

    Add files via upload

---
+++ swap.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Main {
+    public static void main (String[]args)
+    {
+        Scanner sc=new Scanner (System.in);
+        int a=10;
+        int b=20;
+         a=a+b;//30
+         b=a-b;//10
+         a=a-b;//20
+   System.out.println(+a);
+   System.out.println(+b);
+}
+}
